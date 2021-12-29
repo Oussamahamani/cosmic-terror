ee4d62ea3bcf47118da469ba8e70aa5128377dbe
oussamahamani <oussamahamani2000@gmail.com>
2021-12-28 20:09:52 -0500

    adding shooting, created levels and game complete

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ const canvas_height = canvas.height = 689;
 //player coding
 const playerImage = new Image();
 playerImage.src = 'ships/bleu.png';
+const earth = new Image();
+earth.src = 'objects/earth.png';
 const spriteWidth = 192;
 const spriteHeight = 224;
 let framex = 0;
@@ -17,7 +19,7 @@ let frameY = 0;
 let gameFrame = 0;
 const staggerFrames = 5;
 let frametime = 100;
-counter = -1;
+counter = 0;
 
 
 const player = {
@@ -91,6 +93,7 @@ function moveLeft() {
   player.dx = -player.speed;
 }
 
+
 function keyDown(e) {
   if (e.key === 'ArrowRight' || e.key === 'Right') {
     moveRight();
@@ -100,6 +103,8 @@ function keyDown(e) {
     moveUp();
   } else if (e.key === 'ArrowDown' || e.key === 'Down') {
     moveDown();
+  } else if (e.key === 81 || e.key === 'q'){
+    destroy()
   }
 }
 
@@ -112,7 +117,9 @@ function keyUp(e) {
     e.key == 'Up' ||
     e.key == 'ArrowUp' ||
     e.key == 'Down' ||
-    e.key == 'ArrowDown'
+    e.key == 'ArrowDown' ||
+    e.key == 81 ||
+    e.key == 'q'
   ) {
     player.dx = 0;
     player.dy = 0;
@@ -129,12 +136,12 @@ function getRandomInt(min, max) {
 }
 
 class Enemy {
-  constructor(image, spriteWidth, spriteHeight, frames, slow, size,name,x) {
+  constructor(image, spriteWidth, spriteHeight, frames, slow, size,name,x,y,speed) {
     this.image = new Image();
     this.image.src = image;
     this.x = x || Math.random() * canvas_width;
-    this.y = -100;
-    this.speed = getRandomInt(1, 3)
+    this.y = y || -200;
+    this.speed = speed || getRandomInt(1, 3)
     this.spriteWidth = spriteWidth;
     this.spriteHeight = spriteHeight;
     this.width = this.spriteWidth * size;
@@ -158,6 +165,35 @@ class Enemy {
 
   }
 }
+class shooting {
+  constructor(image, spriteWidth, spriteHeight, frames, slow, size) {
+    this.image = new Image();
+    this.image.src = image;
+    this.x = player.x;
+    this.y = player.y;
+    this.speed = 1
+    this.spriteWidth = spriteWidth;
+    this.spriteHeight = spriteHeight;
+    this.width = this.spriteWidth * size;
+    this.height = this.spriteHeight * size;
+    this.frame = 0;
+    this.frames = frames;
+    this.slow = slow
+  }
+  update() {
+    // this.x++;
+    this.y -= 1;
+    if (enemyframe % this.slow === 0) {
+        this.frame > this.frames ? this.frame = 0 : this.frame++;
+    }
+}
+draw() {
+
+    ctx.drawImage(this.image, 0, this.frame * this.spriteHeight, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
+
+  }
+
+}
 //enemies classes
 rock = {
   image: 'objects/rock.png',
@@ -167,10 +203,10 @@ rock = {
   slow: 3,
   size: 0.3,
   number: 6,
-  name: 'rock'
+  name: 'rock',
 }
 octopus = {
-  image: 'objects/octopus.png',
+    image: 'objects/octopus.png',
   width: 256,
   height: 256,
   frames: 6,
@@ -179,21 +215,24 @@ octopus = {
   number: 6,
 }
 fireball = {
-  image: 'objects/fireball.png',
-  width: 586,
-  height: 716,
-  frames: 19,
-  slow: 3,
-  size: 0.25,
-  number: 6,
+    image: 'objects/fireball.png',
+    width: 586,
+    height: 716,
+    frames: 17,
+    slow: 3,
+    size: 1.5,
+    number: 6,
+    x: getRandomInt(0, 1),
+    y: -700,
+    name:'fireball'
 }
 fire = {
-  image: 'objects/fire.png',
-  width: 449,
-  height: 448,
-  frames: 8,
-  slow: 8,
-  size: 0.6,
+    image: 'objects/fire.png',
+    width: 449,
+    height: 448,
+    frames: 8,
+    slow: 8,
+    size: 0.6,
   number: 6,
 }
 wing = {
@@ -206,45 +245,47 @@ wing = {
   number: 6,
 }
 eyes = {
-  image: 'objects/eyes.png',
-  width: 320,
-  height: 320,
-  frames: 200,
-  slow: 4,
-  size: 0.8,
-  number: 1,
-  name: 'eyes'
+    image: 'objects/eyes.png',
+    width: 320,
+    height: 320,
+    frames: 200,
+    slow: 2,
+    size: 0.8,
+    number: 1,
+    name: 'eyes',
+    speed:0.3,
 }
 boss = {
-  image: 'ships/boss.png',
-  width: 528,
-  height: 358,
-  frames: 2,
-  slow: 1,
-  size: 1,
-  number: 1,
+    image: 'ships/boss.png',
+    width: 528,
+    height: 358,
+    frames: 2,
+    slow: 1,
+    size: 1,
+    number: 1,
 }
 alien = {
-  image: 'ships/alien.png',
-  width: 400,
-  height: 400,
-  frames: 33,
-  slow: 6,
-  size: 0.5,
-  number: 6,
+    image: 'ships/alien.png',
+    width: 400,
+    height: 400,
+    frames: 33,
+    slow: 6,
+    size: 0.5,
+    number: 6,
 }
 bob = {
-  image: 'ships/bob.png',
-  width: 400,
-  height: 400,
-  frames: 23,
-  slow: 6,
-  size: 0.3,
-  number: 3,
-  name:'bob'
+    image: 'ships/bob.png',
+    width: 400,
+    height: 400,
+    frames: 23,
+    slow: 6,
+    size: 0.3,
+    number: 3,
+    name:'bob'
 }
 
 
+const beams = [];
 
 //enemies function
 
@@ -252,35 +293,103 @@ bob = {
 // game function and levels
 
 //objects showing on screeen
-function howmany(object){
+// function shoot (){
+    //   enemiesArray.push(new Enemy(object.image, object.width, object.height, object.frames, object.slow, object.size, object.name,player.w,player.y));
+// }
 
-  for (let i = 0; i < object.number; i++) {
-    enemiesArray.push(new Enemy(object.image, object.width, object.height, object.frames, object.slow, object.size, object.name));
+
+function wave(object,number){
+
+  for (let i = 0; i < number; i++) {
+    enemiesArray.push(new Enemy(object.image, object.width, object.height, object.frames, object.slow, object.size, object.name,object.x,object.y));
     // how it works ('objects/x.png',width,heigh,frames,slow,size,x,y)
   }
 
 }
-//levels and the order of the objects showing 
+//levels and the order of the objects showing
 setInterval(() => {
-if (counter <5){  howmany(bob)}
-else {howmany(rock);
-  document.body.style.backgroundRepeat = 'inherit';
-  document.body.style.backgroundSize = '30%';
-  // document.body.style.backgroundImage = "url('pictures/satic1.gif')";
-}
+  if (counter === 10 ){ gun = gun+1}
+  else if (counter === 20 ){ gun = gun+1}
+  else if (counter === 40 ){ gun = gun+1}
+  else if  (counter === 70 ){ gun = gun+1}
+  else if  (counter === 100 ){ gun = gun+1}
+  else if  (counter === 120 ){ gun = gun+1}
+  else if  (counter === 140 ){ gun = gun+1}
+  else if  (counter === 180 ){ gun = gun+1}
+  else if  (counter === 200 ){ gun = gun+1}
+
+  if ( counter < 10){ wave(rock,4) }
+  else if ( counter > 10 && counter < 25 ){  wave(rock,11) }
+  else if (  counter > 20  && counter < 40  ) {  wave(fireball,1) }
+  else if (  counter > 40  && counter < 70  ) {  wave(alien,6); document.body.style.backgroundImage = "url('pictures/2.gif') " }
+  else if (  counter > 70  && counter < 100  ) {  wave(octopus,6); document.body.style.backgroundImage = "url('pictures/invert.gif') " }
+  else if (  counter > 100  && counter < 120  ) {  wave(bob,4); document.body.style.backgroundImage = "url('pictures/background.gif') " }
+  else if (counter > 120 && counter < 140) {  wave(bob,5);document.body.style.backgroundRepeat = 'inherit';document.body.style.backgroundSize = '48%'; }
+  else if (counter > 140 && counter < 160) {  wave(bob,6);document.body.style.backgroundRepeat = 'inherit';document.body.style.backgroundSize = '35%'; }
+  else if (counter > 160 && counter < 180) {  wave(bob,7);document.body.style.backgroundRepeat = 'inherit';document.body.style.backgroundSize = '15%'; }
+  else if (counter > 180 && counter < 200) {  wave(bob,8);document.body.style.backgroundRepeat = 'inherit';document.body.style.backgroundSize = '1%'; }
+  else if (  counter > 200  && counter < 240  ) {  wave(eyes,2); document.body.style.backgroundImage = "url('pictures/satic2.gif')";document.body.style.backgroundRepeat = 'no-repeat';document.body.style.backgroundSize = 'cover'; }
+  //  if (  counter < 100) { document.body.style.backgroundImage = "url('pictures/satic1.gif')"; }
+
+  else if  (counter > 240 && counter < 250){ document.body.style.backgroundImage = "url('pictures/satic1.gif')"}
+  else if  (counter > 250){ alert("you have received a transmition from unkown source, would you like to accept it");popup()}
+
+    function popup(){
+   
+
+      document.querySelector(".popups").style.display = "block"
+      function typeWriter() {
+        if (i < txt.length) {
+
+          
+          document.getElementById("text").innerText += txt.charAt(i);
+          i++;
+          setTimeout(typeWriter, speed);
+        }
+      }
+        typeWriter()
+
+      };
+      
+
 }, 1000)
 
 
-var lives = 20
+// }
+var lives = 5
 //spawn and detection
 function spawn() {
-  enemiesArray.forEach((enemy,index) => {
-    enemy.update();
-    enemy.draw();
+
+
+    
+    enemiesArray.forEach((enemy,index) => {
+        enemy.update();
+        enemy.draw();
+        beams.forEach(beam => {
+            beam.update();
+            beam.draw();
+        
+            //working code
+            if(beam.x > enemy.x + enemy.width ||
+                //right
+                beam.x + beam.w < enemy.x  ||
+                //down
+                beam.y  > enemy.y + enemy.height ||
+            //up
+                beam.y + beam.h< enemy.y){
+                }
+                //but if you touch
+                else{enemiesArray.splice(index,1)}
+
+            })
+
+    
     if (enemiesArray[0].y > canvas_height) { enemiesArray.shift() }
+    if (beams[0]) { if (beams[0].y < 0) { beams.shift() }}
+  
+            
 
-
-    if (enemy.name === 'bob' ){ 
+    if (enemy.name === 'bob' ){
 
       if(player.x +10> enemy.x + enemy.width ||
         //right
@@ -290,10 +399,10 @@ function spawn() {
     //up
         player.y -5 + player.h< enemy.y){
 ///nothing happend if the you didn t touch anything
-        } 
-        //but if you touch 
+        }
+        //but if you touch
         else {if (lives <=0) alert('you have lost')
-        else {enemiesArray.splice(index,1); lives--} 
+        else {enemiesArray.splice(index,1); lives--}
             }
 
 //eyes detection
@@ -307,11 +416,28 @@ function spawn() {
     //up
     player.y -60 + player.h< enemy.y){
 ///nothing happend if the you didn t touch anything
-} 
-//but if you touch 
+}
+//but if you touch
 else {
   if (lives <=0) alert('you have lost') //either you lose
   else {enemiesArray.splice(index,1); lives--} //you lose a life
+    }
+  
+    } else if (enemy.name === 'fireball') {
+    //right
+    if(player.x +75  > enemy.x + enemy.width ||
+    //left
+    player.x + player.w < enemy.x +60 ||
+    //down
+    player.y +70> enemy.y + enemy.height ||
+    //up
+    player.y -60 + player.h< enemy.y){
+///nothing happend if the you didn t touch anything
+}
+//but if you touch
+else {
+  if (lives <=0) alert('you have lost') //either you lose
+  else {; lives= (lives -0.1)} //you lose a life
     }
   }
     else {
@@ -324,57 +450,76 @@ else {
     //up
     player.y -15 + player.h< enemy.y){
 ///nothing happend if the you didn t touch anything
-} 
-//but if you touch 
+}
+//but if you touch
 else {
   if (lives <=0) alert('you have lost') //either you lose
   else {enemiesArray.splice(index,1); lives--} //you lose a life
     }
   }
+})
 
-    })
 }
+    for (let i =0 ; i< enemiesArray.length;i++){
 
+}
 function score() {
   ctx.fillStyle = "white";
   ctx.font = "25px arial";
   ctx.fillText('your score is ' +gameFrame, 10, 40)
-  ctx.fillText('lives left you have: ' +(lives+1), 10, 80)
+  ctx.fillText('lives left you have: ' +(Math.floor(lives+1)), 10, 80)
+  ctx.fillText('shots you have left ' + (gun+1), 10, 120)
 }
+ 
+let gun = 3
 
-// if (counter > 10) document.body.style.backgroundImage = "url('pictures/satic1.gif')";
 
-// main function
 function update() {
   clear();
   drawPlayer();
   newPos();
   score()
   spawn()
+///earth   ctx.drawImage(earth, 600, 250, (488/3), (511/3));
 
+  document.body.onkeyup = function(e){
+    if(e.keyCode  == 32){
+        if (gun >= 0 ){
+      beams.push( new shooting('objects/beam.png',172,200,1,8,0.8)); gun--;
+        } else if (console.log('no ammo'))
+      beams.forEach((beam,index) => {
+          
+      })
+
+  }}
+ 
   enemyframe++;
   requestAnimationFrame(update);
 
 }
 const body = document.querySelector('body ')
 console.log(body.getAttribute('bg-image'))
-//story pop up
 document.addEventListener('keydown', keyDown);
 document.addEventListener('keyup', keyUp);
 
-update()
 
-// window.addEventListener("load", function(){
-//   setTimeout(
-//       function open(event){
-//           document.querySelector(".popup").style.display = "block";
-//       },
-//       1000
-//   )
-// });
-// document.querySelector("#close").addEventListener("click", function(){
-//   document.querySelector(".popup").style.display = "none";update()
-// });
+
+var i = 0;
+var txt = document.getElementById("text").innerHTML; /* The text */
+document.getElementById("text").innerHTML = 'program loading...'
+var speed = 800; 
+window.addEventListener("load", function(){
+  setTimeout(
+      function open(event){
+          document.querySelector(".popup").style.display = "block";
+          
+      },
+      500
+  )
+});
+document.querySelector("#close").addEventListener("click", function(){
+  document.querySelector(".popup").style.display = "none";update()
+});
 
 
 
