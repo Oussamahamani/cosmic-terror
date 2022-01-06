632d984fbb49639f43d8e24900d681d5f6a9b7e1
Oussamahamani <93837468+Oussamahamani@users.noreply.github.com>
2022-01-06 12:11:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -608,7 +608,7 @@ function gameover(){
 counter = -1000;enemiesArray.length = 0; beams.length = 0; lasers.length = 0;
   document.querySelector("#restart").addEventListener("click", function(){
     document.querySelector("#gameover > .popup").style.display = "none";
-    counter= 0; gun = 3 ; lives = 19; gameFrame = 0; lives++;
+    counter= 0; gun = 3 ; lives = 100; gameFrame = 0; lives++;
 
 })
 }
